8411b2814f46b67f93481f0ffbd32604d053ed3b
Digitelektro <32802255+Digitelektro@users.noreply.github.com>
2017-12-18 18:43:09 +0100

    Code pretify

--- framework/driver/gfx/controller/ili9341/src/drv_gfx_ili9341.c
+++ framework/driver/gfx/controller/ili9341/src/drv_gfx_ili9341.c
@@ -81,22 +81,32 @@ static DRV_GFX_ILI9341_OBJ        drvILI9341Obj;
 
 static DRV_GFX_CLIENT_OBJ drvILI9341Clients;
 
-void GFX_TMR_DelayMS(unsigned int delayMs);
+void GFX_TMR_DelayMS(uint16_t delayMs);
+void GFX_TMR_DelayuS(uint16_t delayus);
 
 #define  LCD_DataPort LATB    
-/*
-#define LCD_RS		LATEbits.LATE7  		 
-#define LCD_WR		LATEbits.LATE6  		  
-#define LCD_RD		LATEbits.LATE5  			     
-#define LCD_CS		LATGbits.LATG7  			
-#define LCD_REST	LATGbits.LATG8	  
-*/
 
-void LCD_Writ_Bus(uint16_t data)   
+
+
+/*
+Function: ILI9341_Write_Bus(uint16_t data)  
+
+Summary:
+ Send data to ILI9341
+ 
+Input:
+ Data to be written
+
+*/
+void ILI9341_Write_Bus(uint16_t data)   
 {
+    //PLIB_PORTS_PinClear(PORTS_ID_0, LCD_CS_PORT, LCD_CS_BIT_POS);
 #ifdef GFX_DRV_ILI9341_MODE_SPI
-    DRV_SPI_BUFFER_HANDLE spiBufferHandle = DRV_SPI_BufferAddWrite(drvILI9341Obj.drvSPIHandle, &data, 1, 0,0);
-    while(DRV_SPI_BUFFER_EVENT_COMPLETE != DRV_SPI_BufferStatus (spiBufferHandle));
+    //DRV_SPI_BUFFER_HANDLE spiBufferHandle = DRV_SPI_BufferAddWrite(drvILI9341Obj.drvSPIHandle, &data, 1, 0,0);
+    //while(DRV_SPI_BUFFER_EVENT_COMPLETE != DRV_SPI_BufferStatus (spiBufferHandle));
+    uint8_t received;
+    //Use Faster Blocking function to send data
+    DRV_SPI0_BufferAddWriteRead(&data, &received, 1);
 #endif
     
 #ifdef GFX_DRV_ILI9341_MODE_16BIT
@@ -104,54 +114,97 @@ void LCD_Writ_Bus(uint16_t data)
     LATEbits.LATE3 = data & 0b01;
     data = data >> 1;
     LATEbits.LATE4 = data & 0b01;
-	//LCD_WR=0;
-	//LCD_WR=1; 
 	PLIB_PORTS_PinClear(PORTS_ID_0, LCD_WR_PORT, LCD_WR_BIT_POS);
 	PLIB_PORTS_PinSet(PORTS_ID_0, LCD_WR_PORT, LCD_WR_BIT_POS);
 #endif
+    //PLIB_PORTS_PinSet(PORTS_ID_0, LCD_CS_PORT, LCD_CS_BIT_POS);
 }
 
 
+/*
+Function: ILI9341_WR_DATA(uint16_t data)
 
-void LCD_WR_DATA(uint16_t data)
+Summary:
+ Write ILI9341 data register
+ 
+Input:
+ Data to be written
+
+*/
+void ILI9341_WR_DATA(uint16_t data)
 {
-    //LCD_RS=1;
 	PLIB_PORTS_PinSet(PORTS_ID_0, LCD_RS_PORT, LCD_RS_BIT_POS);
-	LCD_Writ_Bus(data);
+    ILI9341_Write_Bus(data);
 }	
-  
-void LCD_WR_REG(uint16_t data)	 
+ 
+/*
+Function: ILI9341_WR_REG(uint16_t data)
+
+Summary:
+ Write ILI9341 command register
+ 
+Input:
+ Data to be written
+
+*/
+void ILI9341_WR_REG(uint16_t data)	 
 {	
-    //LCD_RS=0;
 	PLIB_PORTS_PinClear(PORTS_ID_0, LCD_RS_PORT, LCD_RS_BIT_POS);
-	LCD_Writ_Bus(data);
+	ILI9341_Write_Bus(data);
 }
 
+/*
+Function: ILI9341_Address_Set(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
 
-void Address_set( uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
+Summary:
+ Set ILI9341 drawing window area
+ 
+Input:
+ Window coordinates
+
+*/
+void ILI9341_Address_Set(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
 { 
-	LCD_WR_REG(0x2A);
-	LCD_WR_DATA(x1>>8);
-	LCD_WR_DATA(x1 & 0xFF);
-	LCD_WR_DATA(x2>>8);
-	LCD_WR_DATA(x2 & 0xFF);
+	ILI9341_WR_REG(0x2A);
+	ILI9341_WR_DATA(x1>>8);
+	ILI9341_WR_DATA(x1 & 0xFF);
+	ILI9341_WR_DATA(x2>>8);
+	ILI9341_WR_DATA(x2 & 0xFF);
 	
-	LCD_WR_REG(0x2B);
-	LCD_WR_DATA(y1>>8);
-	LCD_WR_DATA(y1 & 0xFF);
-	LCD_WR_DATA(y2>>8);
-	LCD_WR_DATA(y2 & 0xFF);	
-	LCD_WR_REG(0x2c);		
+	ILI9341_WR_REG(0x2B);
+	ILI9341_WR_DATA(y1>>8);
+	ILI9341_WR_DATA(y1 & 0xFF);
+	ILI9341_WR_DATA(y2>>8);
+	ILI9341_WR_DATA(y2 & 0xFF);	
+	ILI9341_WR_REG(0x2c);		
 }
 
-void Color_set(uint16_t Color)
+/*
+Function: ILI9341_Color_Set(uint16_t Color)
+
+Summary:
+ Set pixel color
+ 
+Input:
+    Color
+
+*/
+void ILI9341_Color_Set(uint16_t Color)
 {
 #ifdef GFX_DRV_ILI9341_MODE_SPI
-    LCD_WR_DATA(Color>>8);
+    ILI9341_WR_DATA(Color>>8);
 #endif
-	LCD_WR_DATA(Color);
+	ILI9341_WR_DATA(Color);
 }
 
+// *****************************************************************************
+/*
+Function: ILI9341_Init()
+
+Summary:
+    Initialize ILI9341 LCD controller
+
+*/
 void ILI9341_Init()
 {
 	PLIB_PORTS_PinSet(PORTS_ID_0, LCD_CS_PORT, LCD_CS_BIT_POS);
@@ -164,257 +217,128 @@ void ILI9341_Init()
 	PLIB_PORTS_PinSet(PORTS_ID_0, LCD_REST_PORT, LCD_REST_BIT_POS);
 	GFX_TMR_DelayMS(20);	
 	PLIB_PORTS_PinClear(PORTS_ID_0, LCD_CS_PORT, LCD_CS_BIT_POS);
-   
 	//************* Start Initial Sequence **********//
-	LCD_WR_REG(0xcf); 
-	LCD_WR_DATA(0x00);
-	LCD_WR_DATA(0xc1);
-	LCD_WR_DATA(0x30);
+	ILI9341_WR_REG(0xcf); 
+	ILI9341_WR_DATA(0x00);
+	ILI9341_WR_DATA(0xc1);
+	ILI9341_WR_DATA(0x30);
 	
-	LCD_WR_REG(0xed); 
-	LCD_WR_DATA(0x64);
-	LCD_WR_DATA(0x03);
-	LCD_WR_DATA(0x12);
-	LCD_WR_DATA(0x81);
+	ILI9341_WR_REG(0xed); 
+	ILI9341_WR_DATA(0x64);
+	ILI9341_WR_DATA(0x03);
+	ILI9341_WR_DATA(0x12);
+	ILI9341_WR_DATA(0x81);
 	
-	LCD_WR_REG(0xcb); 
-	LCD_WR_DATA(0x39);
-	LCD_WR_DATA(0x2c);
-	LCD_WR_DATA(0x00);
-	LCD_WR_DATA(0x34);
-	LCD_WR_DATA(0x02);
+	ILI9341_WR_REG(0xcb); 
+	ILI9341_WR_DATA(0x39);
+	ILI9341_WR_DATA(0x2c);
+	ILI9341_WR_DATA(0x00);
+	ILI9341_WR_DATA(0x34);
+	ILI9341_WR_DATA(0x02);
 	
-	LCD_WR_REG(0xea); 
-	LCD_WR_DATA(0x00);
-	LCD_WR_DATA(0x00);
+	ILI9341_WR_REG(0xea); 
+	ILI9341_WR_DATA(0x00);
+	ILI9341_WR_DATA(0x00);
 	
-	LCD_WR_REG(0xe8); 
-	LCD_WR_DATA(0x85);
-	LCD_WR_DATA(0x10);
-	LCD_WR_DATA(0x79);
+	ILI9341_WR_REG(0xe8); 
+	ILI9341_WR_DATA(0x85);
+	ILI9341_WR_DATA(0x10);
+	ILI9341_WR_DATA(0x79);
 	
-	LCD_WR_REG(0xC0); //Power control
-	LCD_WR_DATA(0x23); //VRH[5:0]
+	ILI9341_WR_REG(0xC0); //Power control
+	ILI9341_WR_DATA(0x23); //VRH[5:0]
 	
-	LCD_WR_REG(0xC1); //Power control
-	LCD_WR_DATA(0x11); //SAP[2:0];BT[3:0]
+	ILI9341_WR_REG(0xC1); //Power control
+	ILI9341_WR_DATA(0x11); //SAP[2:0];BT[3:0]
 	
-	LCD_WR_REG(0xC2);
-	LCD_WR_DATA(0x11);
+	ILI9341_WR_REG(0xC2);
+	ILI9341_WR_DATA(0x11);
 	
-	LCD_WR_REG(0xC5); //VCM control
-	LCD_WR_DATA(0x3d);
-	LCD_WR_DATA(0x30);
+	ILI9341_WR_REG(0xC5); //VCM control
+	ILI9341_WR_DATA(0x3d);
+	ILI9341_WR_DATA(0x30);
 	
-	LCD_WR_REG(0xc7); 
-	LCD_WR_DATA(0xaa);
+	ILI9341_WR_REG(0xc7); 
+	ILI9341_WR_DATA(0xaa);
 	
-	LCD_WR_REG(0x3A); 
-	LCD_WR_DATA(0x55);
+	ILI9341_WR_REG(0x3A); 
+	ILI9341_WR_DATA(0x55);
 	
-	LCD_WR_REG(0x36); // Memory Access Control, screen rotation
+	ILI9341_WR_REG(0x36); // Memory Access Control, screen rotation
     switch(drvILI9341Obj.initData->orientation)
     {
         case 90:
-            LCD_WR_DATA(0x08);
+            ILI9341_WR_DATA(0x08);
             break;
         case 180:
-            LCD_WR_DATA(0x68);
+            ILI9341_WR_DATA(0x68);
             break;
         case 270:
-            LCD_WR_DATA(0x88);
+            ILI9341_WR_DATA(0x88);
             break;
         default:
-            LCD_WR_DATA(0xA8);
+            ILI9341_WR_DATA(0xA8);
             break;
                 
     }
 
-	LCD_WR_REG(0xB1); // Frame Rate Control
-	LCD_WR_DATA(0x00);
-	LCD_WR_DATA(0x11);
+	ILI9341_WR_REG(0xB1); // Frame Rate Control
+	ILI9341_WR_DATA(0x00);
+	ILI9341_WR_DATA(0x11);
 	
-	LCD_WR_REG(0xB6); // Display Function Control
-	LCD_WR_DATA(0x0a);
-	LCD_WR_DATA(0xa2);
+	ILI9341_WR_REG(0xB6); // Display Function Control
+	ILI9341_WR_DATA(0x0a);
+	ILI9341_WR_DATA(0xa2);
 	
-	LCD_WR_REG(0xF2); // 3Gamma Function Disable
-	LCD_WR_DATA(0x00);
+	ILI9341_WR_REG(0xF2); // 3Gamma Function Disable
+	ILI9341_WR_DATA(0x00);
 	
-	LCD_WR_REG(0xF7);
-	LCD_WR_DATA(0x20);
+	ILI9341_WR_REG(0xF7);
+	ILI9341_WR_DATA(0x20);
 	
-	LCD_WR_REG(0xF1);
-	LCD_WR_DATA(0x01);
-	LCD_WR_DATA(0x30);
+	ILI9341_WR_REG(0xF1);
+	ILI9341_WR_DATA(0x01);
+	ILI9341_WR_DATA(0x30);
 	
-	LCD_WR_REG(0x26); //Gamma curve selected
-	LCD_WR_DATA(0x01);
+	ILI9341_WR_REG(0x26); //Gamma curve selected
+	ILI9341_WR_DATA(0x01);
 	
-	LCD_WR_REG(0xE0); //Set Gamma
-	LCD_WR_DATA(0x0f);
-	LCD_WR_DATA(0x3f);
-	LCD_WR_DATA(0x2f);
-	LCD_WR_DATA(0x0c);
-	LCD_WR_DATA(0x10);
-	LCD_WR_DATA(0x0a);
-	LCD_WR_DATA(0x53);
-	LCD_WR_DATA(0xd5);
-	LCD_WR_DATA(0x40);
-	LCD_WR_DATA(0x0a);
-	LCD_WR_DATA(0x13);
-	LCD_WR_DATA(0x03);
-	LCD_WR_DATA(0x08);
-	LCD_WR_DATA(0x03);
-	LCD_WR_DATA(0x00);
+	ILI9341_WR_REG(0xE0); //Set Gamma
+	ILI9341_WR_DATA(0x0f);
+	ILI9341_WR_DATA(0x3f);
+	ILI9341_WR_DATA(0x2f);
+	ILI9341_WR_DATA(0x0c);
+	ILI9341_WR_DATA(0x10);
+	ILI9341_WR_DATA(0x0a);
+	ILI9341_WR_DATA(0x53);
+	ILI9341_WR_DATA(0xd5);
+	ILI9341_WR_DATA(0x40);
+	ILI9341_WR_DATA(0x0a);
+	ILI9341_WR_DATA(0x13);
+	ILI9341_WR_DATA(0x03);
+	ILI9341_WR_DATA(0x08);
+	ILI9341_WR_DATA(0x03);
+	ILI9341_WR_DATA(0x00);
 	
-	LCD_WR_REG(0xE1); //Set Gamma
-	LCD_WR_DATA(0x00);
-	LCD_WR_DATA(0x00);
-	LCD_WR_DATA(0x10);
-	LCD_WR_DATA(0x03);
-	LCD_WR_DATA(0x0f);
-	LCD_WR_DATA(0x05);
-	LCD_WR_DATA(0x2c);
-	LCD_WR_DATA(0xa2);
-	LCD_WR_DATA(0x3f);
-	LCD_WR_DATA(0x05);
-	LCD_WR_DATA(0x0e);
-	LCD_WR_DATA(0x0c);
-	LCD_WR_DATA(0x37);
-	LCD_WR_DATA(0x3c);
-	LCD_WR_DATA(0x0F);
-	LCD_WR_REG(0x11); 
+	ILI9341_WR_REG(0xE1); //Set Gamma
+	ILI9341_WR_DATA(0x00);
+	ILI9341_WR_DATA(0x00);
+	ILI9341_WR_DATA(0x10);
+	ILI9341_WR_DATA(0x03);
+	ILI9341_WR_DATA(0x0f);
+	ILI9341_WR_DATA(0x05);
+	ILI9341_WR_DATA(0x2c);
+	ILI9341_WR_DATA(0xa2);
+	ILI9341_WR_DATA(0x3f);
+	ILI9341_WR_DATA(0x05);
+	ILI9341_WR_DATA(0x0e);
+	ILI9341_WR_DATA(0x0c);
+	ILI9341_WR_DATA(0x37);
+	ILI9341_WR_DATA(0x3c);
+	ILI9341_WR_DATA(0x0F);
+	ILI9341_WR_REG(0x11); 
 	GFX_TMR_DelayMS(80);
-	LCD_WR_REG(0x29); //display on
-    
- /*
-    LCD_WR_REG(0xEF);
-    LCD_WR_DATA(0x03);
-    LCD_WR_DATA(0x80);
-    LCD_WR_DATA(0x02);
-
-    LCD_WR_REG(0xCF);
-    LCD_WR_DATA(0x00);
-    LCD_WR_DATA(0XC1);
-    LCD_WR_DATA(0X30);
-
-    LCD_WR_REG(0xED);
-    LCD_WR_DATA(0x64);
-    LCD_WR_DATA(0x03);
-    LCD_WR_DATA(0X12);
-    LCD_WR_DATA(0X81);
-
-    LCD_WR_REG(0xE8);
-    LCD_WR_DATA(0x85);
-    LCD_WR_DATA(0x00);
-    LCD_WR_DATA(0x78);
-
-    LCD_WR_REG(0xCB);
-    LCD_WR_DATA(0x39);
-    LCD_WR_DATA(0x2C);
-    LCD_WR_DATA(0x00);
-    LCD_WR_DATA(0x34);
-    LCD_WR_DATA(0x02);
-
-    LCD_WR_REG(0xF7);
-    LCD_WR_DATA(0x20);
-
-    LCD_WR_REG(0xEA);
-    LCD_WR_DATA(0x00);
-    LCD_WR_DATA(0x00);
-
-    LCD_WR_REG(ILI9341_PWCTR1);    //Power control
-    LCD_WR_DATA(0x23);   //VRH[5:0]
-
-    LCD_WR_REG(ILI9341_PWCTR2);    //Power control
-    LCD_WR_DATA(0x10);   //SAP[2:0];BT[3:0]
-
-    LCD_WR_REG(ILI9341_VMCTR1);    //VCM control
-    LCD_WR_DATA(0x3e);
-    LCD_WR_DATA(0x28);
-
-    LCD_WR_REG(ILI9341_VMCTR2);    //VCM control2
-    LCD_WR_DATA(0x86);  //--
-
-    LCD_WR_REG(ILI9341_MADCTL);    // Memory Access Control, screen rotation
-    switch(drvILI9341Obj.initData->orientation)
-    {
-        case 90:
-            LCD_WR_DATA(0x08);
-            break;
-        case 180:
-            LCD_WR_DATA(0x68);
-            break;
-        case 270:
-            LCD_WR_DATA(0x88);
-            break;
-        default:
-            LCD_WR_DATA(0xA8);
-            break;
-                
-    }
-
-    LCD_WR_REG(ILI9341_VSCRSADD); // Vertical scroll
-    LCD_WR_DATA(0, 2);                 // Zero
-
-    LCD_WR_REG(ILI9341_PIXFMT);
-    LCD_WR_DATA(0x55);
-
-    LCD_WR_REG(ILI9341_FRMCTR1);
-    LCD_WR_DATA(0x00);
-    LCD_WR_DATA(0x18);
-
-    LCD_WR_REG(ILI9341_DFUNCTR);    // Display Function Control
-    LCD_WR_DATA(0x08);
-    LCD_WR_DATA(0x82);
-    LCD_WR_DATA(0x27);
-
-    LCD_WR_REG(0xF2);    // 3Gamma Function Disable
-    LCD_WR_DATA(0x00);
-
-    LCD_WR_REG(ILI9341_GAMMASET);    //Gamma curve selected
-    LCD_WR_DATA(0x01);
-
-    LCD_WR_REG(ILI9341_GMCTRP1);    //Set Gamma
-    LCD_WR_DATA(0x0F);
-    LCD_WR_DATA(0x31);
-    LCD_WR_DATA(0x2B);
-    LCD_WR_DATA(0x0C);
-    LCD_WR_DATA(0x0E);
-    LCD_WR_DATA(0x08);
-    LCD_WR_DATA(0x4E);
-    LCD_WR_DATA(0xF1);
-    LCD_WR_DATA(0x37);
-    LCD_WR_DATA(0x07);
-    LCD_WR_DATA(0x10);
-    LCD_WR_DATA(0x03);
-    LCD_WR_DATA(0x0E);
-    LCD_WR_DATA(0x09);
-    LCD_WR_DATA(0x00);
-
-    LCD_WR_REG(ILI9341_GMCTRN1);    //Set Gamma
-    LCD_WR_DATA(0x00);
-    LCD_WR_DATA(0x0E);
-    LCD_WR_DATA(0x14);
-    LCD_WR_DATA(0x03);
-    LCD_WR_DATA(0x11);
-    LCD_WR_DATA(0x07);
-    LCD_WR_DATA(0x31);
-    LCD_WR_DATA(0xC1);
-    LCD_WR_DATA(0x48);
-    LCD_WR_DATA(0x08);
-    LCD_WR_DATA(0x0F);
-    LCD_WR_DATA(0x0C);
-    LCD_WR_DATA(0x31);
-    LCD_WR_DATA(0x36);
-    LCD_WR_DATA(0x0F);
-
-    LCD_WR_REG(ILI9341_SLPOUT);    //Exit Sleep
-    GFX_TMR_DelayMS(120);
-    LCD_WR_REG(ILI9341_DISPON);    //Display on
-    GFX_TMR_DelayMS(120);
-    */
+	ILI9341_WR_REG(0x29); //display on
     
 }
 
@@ -672,65 +596,26 @@ void DRV_GFX_ILI9341_SetInstance(uint8_t instance)
 	instance = instance;
 }
 
-void  DRV_GFX_ILI9341_LineDraw(short x1, short y1, short x2, short y2)
-{
-	uint16_t t; 
-	int16_t xerr=0,yerr=0,delta_x,delta_y,distance; 
-	int16_t incx,incy,uRow,uCol; 
-
-	delta_x=x2-x1; 
-	delta_y=y2-y1; 
-	uRow=x1; 
-	uCol=y1; 
-	if(delta_x>0)incx=1; 
-	else if(delta_x==0)incx=0;
-	else {incx=-1;delta_x=-delta_x;} 
-	if(delta_y>0)incy=1; 
-	else if(delta_y==0)incy=0;
-	else{incy=-1;delta_y=-delta_y;} 
-	if( delta_x>delta_y)distance=delta_x; 
-	else distance=delta_y; 
-	for(t=0;t<=distance+1;t++ )
-	{  
-		DRV_GFX_ILI9341_PixelPut(uRow,uCol);
-		xerr+=delta_x ; 
-		yerr+=delta_y ; 
-		if(xerr>distance) 
-		{ 
-			xerr-=distance; 
-			uRow+=incx; 
-		} 
-		if(yerr>distance) 
-		{ 
-			yerr-=distance; 
-			uCol+=incy; 
-		} 
-	}  
-}
 
 // *****************************************************************************
 /*
 Function: void  DRV_GFX_ILI9341_BarFill(uint16_t left, uint16_t top, uint16_t right, uint16_t bottom)
 
 Summary:
-Hardware accelerated barfill function
+Barfill function
 
 Description:
-see primitive BarDraw
 
-Output:
-1 - call not successful (PMP driver busy)
-0 - call successful
 */
 void  DRV_GFX_ILI9341_BarFill(uint16_t left, uint16_t top, uint16_t right, uint16_t bottom)
 {
     uint16_t i,j; 
-	Address_set(left,top,right,bottom);      
+	ILI9341_Address_Set(left,top,right,bottom);      
 	for(i=top;i<=bottom;i++)
 	{													   	 	
 		for(j=left;j<=right;j++)
         {
-            Color_set(drvILI9341Obj.initData->color); 	   
+            ILI9341_Color_Set(drvILI9341Obj.initData->color); 	   
         }
         Nop();
 	}
@@ -757,8 +642,8 @@ Output:
 */
 void DRV_GFX_ILI9341_PixelPut(uint16_t x, uint16_t y)
 {
-    Address_set(x,y,x,y);
-	Color_set(drvILI9341Obj.initData->color);
+    ILI9341_Address_Set(x,y,x,y);
+	ILI9341_Color_Set(drvILI9341Obj.initData->color);
 
 }
 
@@ -784,13 +669,12 @@ handle to the number of pixels remaining
 void  DRV_GFX_ILI9341_PixelArrayPut(uint16_t *color, uint16_t x, uint16_t y, uint16_t count, uint16_t lineCount)
 {
     uint16_t i,j; 
-	Address_set(x,y,x + count,y + lineCount);      
+	ILI9341_Address_Set(x,y,x + count,y + lineCount);      
 	for(i=0;i<=lineCount;i++)
 	{													   	 	
 		for(j=0;j<=count;j++)
-            Color_set(color[j]); 	    
+            ILI9341_Color_Set(color[j]); 	    
 	} 		
-
 }
 
 // *****************************************************************************
@@ -838,11 +722,9 @@ Output:
 */
 uint16_t  DRV_GFX_ILI9341_Busy(uint8_t instance)
 {
-
 	uint8_t status = 0x00;
 	//Get status
 	return((bool)(!status));
-
 }
 
 // *****************************************************************************
@@ -882,27 +764,42 @@ SYS_STATUS DRV_GFX_ILI9341_Status(SYS_MODULE_OBJ object)
 
 
 
+// *****************************************************************************
+/*
+Function: void  GFX_TMR_DelayMS(uint16_t delayMs)
 
-void GFX_TMR_DelayMS(unsigned int delayMs)
+Summary:
+ * Blocking delay function
+
+*/
+void GFX_TMR_DelayMS(uint16_t delayMs)
 {
-	#if (true == SYS_TMR_INTERRUPT_MODE)
+    if (delayMs)
+    {
+        uint32_t sysClk = SYS_CLK_FREQ;
+        uint32_t t0;
+        t0 = _CP0_GET_COUNT();
+        while (_CP0_GET_COUNT() - t0 < (sysClk / 2000)*delayMs);
+    }
+}
 
-	#else      //Primitive Blocking Mode
-		if (delayMs)
-		{
-			uint32_t sysClk = SYS_CLK_FREQ;
-			uint32_t t0;
-	#if   defined (__C32__)
-			t0 = _CP0_GET_COUNT();
-			while (_CP0_GET_COUNT() - t0 < (sysClk / 2000)*delayMs);
-	#elif defined (__C30__)
-			t0 = ReadTimer23();
-			while (ReadTimer23() - t0 < (sysClk / 2000)*mSec);
-	#else
-	#error "Neither __C32__ nor __C30__ is defined!"
-	#endif
-		}
-	#endif
+// *****************************************************************************
+/*
+Function: void  GFX_TMR_DelayuS(uint16_t delayus)
+
+Summary:
+ * Blocking delay function
+
+*/
+void GFX_TMR_DelayuS(uint16_t delayus)
+{
+	if (delayus)
+    {
+        uint32_t sysClk = SYS_CLK_FREQ;
+        uint32_t t0;
+        t0 = _CP0_GET_COUNT();
+        while (_CP0_GET_COUNT() - t0 < (sysClk / 2000000)*delayus);
+    }
 }
 
 
